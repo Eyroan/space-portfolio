d3687c55c1a6f827c0f892759c6eaacfd6066972
Eyroan <miguelcarian50@gmail.com>
2025-07-29 14:03:35 +0800

    space portfolio change

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Aaron Carian | Personal Portfolio",
+  title: "Aaron Carian | Space Portfolio",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
